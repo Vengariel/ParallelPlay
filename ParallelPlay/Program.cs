4fc582ce1445ab9e6a65f83b3c13f084f896d8d2
Marco Mendieta <ardenteveritate@live.com>
2019-11-27 10:53:43 -0600

    Initial example

--- ParallelPlay/Program.cs
+++ ParallelPlay/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ParallelPlay
 {
@@ -6,7 +10,43 @@ namespace ParallelPlay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stopwatch sw = null;
+
+            long sum = 0;
+            long SUMTOP = 10000000;
+
+            // Try sequential for
+            sw = Stopwatch.StartNew();
+            for (long i = 0; i < SUMTOP; i++) sum += i;
+            sw.Stop();
+            Console.WriteLine("sequential for result = {0}, time = {1} ms", sum, sw.ElapsedMilliseconds);
+            Console.ReadKey();
+            // Try parallel for -- this is slow!
+            sum = 0;
+            sw = Stopwatch.StartNew();
+            Parallel.For(0L, SUMTOP, (item) => Interlocked.Add(ref sum, item));
+            sw.Stop();
+            Console.WriteLine("parallel for  result = {0}, time = {1} ms", sum, sw.ElapsedMilliseconds);
+            Console.ReadKey();
+            // Try parallel for with locals
+            sum = 0;
+            sw = Stopwatch.StartNew();
+            Parallel.For(0L, SUMTOP, () => 0L, (item, state, prevLocal) => prevLocal + item, local => Interlocked.Add(ref sum, local));
+            sw.Stop();
+            Console.WriteLine("parallel for w/locals result = {0}, time = {1} ms", sum, sw.ElapsedMilliseconds);
+            Console.ReadKey();
+            // Try range partitioner
+            sum = 0;
+            sw = Stopwatch.StartNew();
+            Parallel.ForEach(Partitioner.Create(0L, SUMTOP), (range) =>
+            {
+                long local = 0;
+                for (long i = range.Item1; i < range.Item2; i++) local += i;
+                Interlocked.Add(ref sum, local);
+            });
+            sw.Stop();
+            Console.WriteLine("range partitioner result = {0}, time = {1} ms", sum, sw.ElapsedMilliseconds);
+            Console.ReadKey();
         }
     }
 }
