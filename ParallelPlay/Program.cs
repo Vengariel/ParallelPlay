dcbe876deb2ee524f3d86629d922a0bdd4058558
Marco Mendieta <ardenteveritate@live.com>
2019-11-27 10:45:38 -0600

    Add project files.

---
+++ ParallelPlay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParallelPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
